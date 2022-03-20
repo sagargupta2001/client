ee2fbcef9666b09d810e6bd53822d506ee09ac69
Sagar <sagargupta201@outlook.com>
2022-03-20 17:55:20 +0530

    Added pagination component and cleaned up loading indicators in the catalog.

--- src/features/catalog/Catalog.tsx
+++ src/features/catalog/Catalog.tsx
@@ -2,16 +2,14 @@ import {useEffect} from "react";
 import ProductList from "./ProductList";
 import LoadingComponent from "../../App/layout/LoadingComponent";
 import {useAppDispatch, useAppSelector} from "../../App/store/ConfigureStore";
-import {fetchFilters, fetchProductsAsync, productSelectors, setProductParams} from "./catalogSlice";
+import {fetchFilters, fetchProductsAsync, productSelectors, setPageNumber, setProductParams} from "./catalogSlice";
 import {
-    Box,
-    Grid, Pagination,
-    Paper,
-    Typography
+    Grid, Paper
 } from "@mui/material";
 import ProductSearch from "./ProductSearch";
 import RadioButtonGroup from "../../App/components/RadioButtonGroup";
 import CheckboxButtons from "../../App/components/CheckboxButtons";
+import AppPagination from "../../App/components/AppPagination";
 
 const sortOptions = [
     {value: 'name', label: 'Alphabetical'},
@@ -21,7 +19,8 @@ const sortOptions = [
 
 export default function Catalog() {
     const products = useAppSelector(productSelectors.selectAll);
-    const {productsLoaded, status, filtersLoaded, brands, types, productParams} = useAppSelector(state => state.catalog);
+    const {productsLoaded, filtersLoaded, brands, types, productParams, metaData} =
+        useAppSelector(state => state.catalog);
     const dispatch = useAppDispatch();
 
     useEffect(() => {
@@ -32,10 +31,10 @@ export default function Catalog() {
         if (!filtersLoaded) dispatch(fetchFilters());
     }, [dispatch, filtersLoaded])
 
-    if (status.includes('pending')) return <LoadingComponent message='Loading products...'/>
+    if (!filtersLoaded) return <LoadingComponent message='Loading products...'/>
 
     return (
-        <Grid container spacing={4}>
+        <Grid container columnSpacing={4}>
             <Grid item xs={3}>
                 <Paper sx={{mb: 2}}>
                     <ProductSearch />
@@ -67,18 +66,12 @@ export default function Catalog() {
                 <ProductList products={products}/>
             </Grid>
             <Grid item xs={3} />
-            <Grid item xs={9}>
-                <Box display='flex' justifyContent='space-between' alignItems='center'>
-                    <Typography>
-                        Displaying 1-6 of 20 items
-                    </Typography>
-                    <Pagination
-                        color='secondary'
-                        size='large'
-                        count={10}
-                        page={2}
-                    />
-                </Box>
+            <Grid item xs={9} sx={{mb: 2}} >
+                {metaData &&
+                <AppPagination
+                    metaData={metaData}
+                    onPageChange={(page: number) => dispatch(setPageNumber({pageNumber: page}))}
+                />}
             </Grid>
         </Grid>
     )
--- src/features/catalog/ProductList.tsx
+++ src/features/catalog/ProductList.tsx
@@ -2,18 +2,25 @@ import {Grid} from "@mui/material";
 import { Fragment } from "react";
 import {Product} from "../../App/models/products";
 import ProductCard from "./ProductCard";
+import {useAppSelector} from "../../App/store/ConfigureStore";
+import ProductCardSkeleton from "./ProductCardSkeleton";
 
 interface Props {
     products: Product[];
 }
 
 export default function ProductList({products} : Props) {
+    const {productsLoaded} = useAppSelector(state => state.catalog);
     return (
         <Fragment>
             <Grid container spacing={4}>
                 {products.map(product => (
                     <Grid item xs={4} key={product.id}>
-                        <ProductCard product={product}/>
+                        {!productsLoaded ? (
+                            <ProductCardSkeleton />
+                        ) : (
+                            <ProductCard product={product}/>
+                        )}
                     </Grid>
                 ))}
             </Grid>
