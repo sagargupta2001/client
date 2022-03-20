805feb95a0372207c95bd985e6769646088c5334
Sagar <sagargupta201@outlook.com>
2022-03-20 15:31:05 +0530

    Designing catalog page -> added filters and pagination (non-functional).

--- src/features/catalog/Catalog.tsx
+++ src/features/catalog/Catalog.tsx
@@ -1,13 +1,31 @@
-import {Fragment, useEffect} from "react";
+import {useEffect} from "react";
 import ProductList from "./ProductList";
 import LoadingComponent from "../../App/layout/LoadingComponent";
 import {useAppDispatch, useAppSelector} from "../../App/store/ConfigureStore";
 import {fetchFilters, fetchProductsAsync, productSelectors} from "./catalogSlice";
-import { Grid } from "@mui/material";
+import {
+    Box,
+    Checkbox,
+    FormControl,
+    FormControlLabel,
+    FormGroup,
+    FormLabel,
+    Grid, Pagination,
+    Paper,
+    Radio,
+    RadioGroup,
+    TextField, Typography
+} from "@mui/material";
+
+const sortOptions = [
+    {value: 'name', label: 'Alphabetical'},
+    {value: 'priceDesc', label: 'Price - High to Low'},
+    {value: 'price', label: 'Price - Low to High'}
+]
 
 export default function Catalog() {
     const products = useAppSelector(productSelectors.selectAll);
-    const {productsLoaded, status, filtersLoaded} = useAppSelector(state => state.catalog);
+    const {productsLoaded, status, filtersLoaded, brands, types} = useAppSelector(state => state.catalog);
     const dispatch = useAppDispatch();
 
     useEffect(() => {
@@ -23,10 +41,55 @@ export default function Catalog() {
     return (
         <Grid container spacing={4}>
             <Grid item xs={3}>
+                <Paper sx={{mb: 2}}>
+                    <TextField
+                        label='Search products'
+                        variant='outlined'
+                        fullWidth
+                    />
+                </Paper>
+                <Paper sx={{mb: 2, p: 2}}>
+                    <FormControl>
+                        <RadioGroup>
+                            {sortOptions.map(({value, label}) => (
+                                <FormControlLabel value={value} control={<Radio />} label={label} key={value}/>
+                            ))}
+                        </RadioGroup>
+                    </FormControl>
+                </Paper>
 
+                <Paper sx={{mb: 2, p: 2}}>
+                    <FormGroup>
+                        {brands.map(brand => (
+                            <FormControlLabel control={<Checkbox />} label={brand} key={brand} />
+                        ))}
+                    </FormGroup>
+                </Paper>
+
+                <Paper sx={{mb: 2, p: 2}}>
+                    <FormGroup>
+                        {types.map(type => (
+                            <FormControlLabel control={<Checkbox />} label={type} key={type} />
+                        ))}
+                    </FormGroup>
+                </Paper>
             </Grid>
             <Grid item xs={9}>
                 <ProductList products={products}/>
+            </Grid>
+            <Grid item xs={3} />
+            <Grid item xs={9}>
+                <Box display='flex' justifyContent='space-between' alignItems='center'>
+                    <Typography>
+                        Displaying 1-6 of 20 items
+                    </Typography>
+                    <Pagination
+                        color='secondary'
+                        size='large'
+                        count={10}
+                        page={2}
+                    />
+                </Box>
             </Grid>
         </Grid>
     )
