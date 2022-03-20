46c23a7ba162ceef00ab39b0501f13f096d00688
Sagar <sagargupta201@outlook.com>
2022-03-20 14:22:35 +0530

    Getting the filter list from API

--- src/features/catalog/Catalog.tsx
+++ src/features/catalog/Catalog.tsx
@@ -2,22 +2,32 @@ import {Fragment, useEffect} from "react";
 import ProductList from "./ProductList";
 import LoadingComponent from "../../App/layout/LoadingComponent";
 import {useAppDispatch, useAppSelector} from "../../App/store/ConfigureStore";
-import {fetchProductsAsync, productSelectors} from "./catalogSlice";
+import {fetchFilters, fetchProductsAsync, productSelectors} from "./catalogSlice";
+import { Grid } from "@mui/material";
 
 export default function Catalog() {
     const products = useAppSelector(productSelectors.selectAll);
-    const {productsLoaded, status} = useAppSelector(state => state.catalog);
+    const {productsLoaded, status, filtersLoaded} = useAppSelector(state => state.catalog);
     const dispatch = useAppDispatch();
 
     useEffect(() => {
         if (!productsLoaded) dispatch(fetchProductsAsync());
-    }, [dispatch, productsLoaded])
+    }, [productsLoaded, dispatch])
+
+    useEffect(() => {
+        if (!filtersLoaded) dispatch(fetchFilters());
+    }, [dispatch, filtersLoaded])
 
     if (status.includes('pending')) return <LoadingComponent message='Loading products...'/>
 
     return (
-        <Fragment>
-            <ProductList products={products}/>
-        </Fragment>
+        <Grid container spacing={4}>
+            <Grid item xs={3}>
+
+            </Grid>
+            <Grid item xs={9}>
+                <ProductList products={products}/>
+            </Grid>
+        </Grid>
     )
 }
