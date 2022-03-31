a111a938ae24e1ecd83d99a09ab0c68b7429b746
Sagar <sagargupta201@outlook.com>
2022-03-31 20:33:27 +0530

    Added React Hook Form Validators.

--- src/features/account/Login.tsx
+++ src/features/account/Login.tsx
@@ -13,10 +13,16 @@ import agent from "../../App/api/agent";
 import { LoadingButton } from '@material-ui/lab';
 
 export default function Login() {
-    const {register, handleSubmit, formState: {isSubmitting}} = useForm();
+    const {register, handleSubmit, formState: {isSubmitting, errors, isValid}} = useForm({
+        mode: 'onTouched'
+    });
 
     async function submitForm(data: FieldValues) {
-        await agent.Account.login(data);
+        try {
+            await agent.Account.login(data);
+        } catch (error) {
+            console.log(error);
+        }
     }
 
     return (
@@ -38,17 +44,22 @@ export default function Login() {
                             fullWidth
                             label="Username"
                             autoFocus
-                            {...register('username')}
+                            {...register('username', {required: 'Username is required.'})}
+                            error={!!errors.username}
+                            helperText={errors?.username?.message}
                         />
                         <TextField
                             margin="normal"
                             fullWidth
                             label="Password"
                             type="password"
-                            {...register('password')}
+                            {...register('password', {required: 'Password is required.'})}
+                            error={!!errors.password}
+                            helperText={errors?.password?.message}
                         />
                         <LoadingButton
                             loading={isSubmitting}
+                            disabled={!isValid}
                             type="submit"
                             fullWidth
                             variant="contained"
