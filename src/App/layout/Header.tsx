f129700c50701e4cc8a3a184d592835df2fe076e
Sagar <sagargupta201@outlook.com>
2022-04-02 14:58:50 +0530

    Created Checkout Component.

--- src/App/layout/Header.tsx
+++ src/App/layout/Header.tsx
@@ -35,8 +35,8 @@ const navStyles = {
 }
 
 export default function Header({darkMode, handleThemeChange}: Props) {
-    const { basket } = useAppSelector(state => state.basket);
-    const { user } = useAppSelector(state => state.account);
+    const {basket} = useAppSelector(state => state.basket);
+    const {user} = useAppSelector(state => state.account);
     const itemCount = basket?.items.reduce((sum, item) => sum + item.quantity, 0);
 
     return (
@@ -57,7 +57,7 @@ export default function Header({darkMode, handleThemeChange}: Props) {
                             sx={navStyles}>
                             S-Kart
                         </Typography>
-                        <MUISwitch onChange={handleThemeChange} checked={darkMode} />
+                        <MUISwitch onChange={handleThemeChange} checked={darkMode}/>
                     </Box>
 
                     <List sx={{display: 'flex'}}>
@@ -72,30 +72,14 @@ export default function Header({darkMode, handleThemeChange}: Props) {
                             </ListItem>
                         ))}
                     </List>
-                    {user ? (
-                        <SignedInMenu />
-                    ) : (
-                        <List sx={{ display: 'flex' }}>
-                            {rightLinks.map(({ title, path }) => (
-                                <ListItem
-                                    component={NavLink}
-                                    to={path}
-                                    key={path}
-                                    sx={navStyles}
-                                >
-                                    {title.toUpperCase()}
-                                </ListItem>
-                            ))}
-                        </List>
-                    )}
                     <Box display='flex' alignItems='center'>
                         <IconButton component={Link} to='/basket' size='large' sx={{color: 'inherit'}}>
                             <Badge badgeContent={itemCount} color='secondary'>
-                                <ShoppingCart />
+                                <ShoppingCart/>
                             </Badge>
                         </IconButton>
                         {user ? (
-                            <SignedInMenu />
+                            <SignedInMenu/>
                         ) : (
                             <List sx={{display: 'flex'}}>
                                 {rightLinks.map(({title, path}) => (
@@ -111,7 +95,6 @@ export default function Header({darkMode, handleThemeChange}: Props) {
                             </List>
                         )}
                     </Box>
-
                 </Toolbar>
             </AppBar>
         </Fragment>
