266743be0847393716d82bc40d696d1db415d7b8
Sagar <sagargupta201@outlook.com>
2022-04-07 12:31:22 +0530

    Added Stipe payment inputs.

--- src/features/checkout/PaymentForm.tsx
+++ src/features/checkout/PaymentForm.tsx
@@ -1,11 +1,10 @@
 import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Grid';
 import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
 import {useFormContext} from "react-hook-form";
 import AppTextInput from "../../App/components/AppTextInput";
-import {CardNumberElement} from "@stripe/react-stripe-js";
+import {CardCvcElement, CardExpiryElement, CardNumberElement} from "@stripe/react-stripe-js";
+import {StripeInput} from "./StripeInput";
 
 export default function PaymentForm() {
     const {control} = useFormContext();
@@ -24,10 +23,13 @@ export default function PaymentForm() {
                         label="Card number"
                         fullWidth
                         autoComplete="cc-number"
-                        variant="standard"
+                        variant="outlined"
                         InputLabelProps={{shrink: true}}
                         InputProps={{
-                            inputComponent: CardNumberElement
+                            inputComponent: StripeInput,
+                            inputProps: {
+                                component: CardNumberElement
+                            }
                         }}
                     />
                 </Grid>
@@ -38,7 +40,14 @@ export default function PaymentForm() {
                         label="Expiry date"
                         fullWidth
                         autoComplete="cc-exp"
-                        variant="standard"
+                        variant="outlined"
+                        InputLabelProps={{shrink: true}}
+                        InputProps={{
+                            inputComponent: StripeInput,
+                            inputProps: {
+                                component: CardExpiryElement
+                            }
+                        }}
                     />
                 </Grid>
                 <Grid item xs={12} md={6}>
@@ -49,13 +58,14 @@ export default function PaymentForm() {
                         helperText="Last three digits on signature strip"
                         fullWidth
                         autoComplete="cc-csc"
-                        variant="standard"
-                    />
-                </Grid>
-                <Grid item xs={12}>
-                    <FormControlLabel
-                        control={<Checkbox color="secondary" name="saveCard" value="yes" />}
-                        label="Remember credit card details for next time"
+                        variant="outlined"
+                        InputLabelProps={{shrink: true}}
+                        InputProps={{
+                            inputComponent: StripeInput,
+                            inputProps: {
+                                component: CardCvcElement
+                            }
+                        }}
                     />
                 </Grid>
             </Grid>
